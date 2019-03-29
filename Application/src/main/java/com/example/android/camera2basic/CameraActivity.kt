9bdcbfa6f449eab8fa2c3c2cd218b51baea8a564
Tim Olson <mr.kaito01@gmail.com>
2019-03-29 23:26:13 +0900

    delete log

--- Application/src/main/java/com/example/android/camera2basic/CameraActivity.kt
+++ Application/src/main/java/com/example/android/camera2basic/CameraActivity.kt
@@ -2,7 +2,6 @@ package com.example.android.camera2basic
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import org.opencv.android.OpenCVLoader
 import java.io.File
 
@@ -15,9 +14,7 @@ class CameraActivity : AppCompatActivity() {
                 .replace(R.id.container, Camera2BasicFragment.newInstance())
                 .commit()
 
-        if (OpenCVLoader.initDebug()){
-            Log.i("SUCCESS","OK")
-        }
+        OpenCVLoader.initDebug()
 
         val dir = File("/storage/emulated/0/DCIM/Yakudo/")
         if (!dir.exists()){
